1893a379683eea84e747c641bad47fe6ddb84a32
Kenneth Jung <jungeu1509@gmail.com>
2022-09-13 21:26:44 +0900

    feature/findbyid

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -23,16 +23,24 @@ public class PostController {
     }
 
     // 2-1. 글 목록을 조회한다.
+    // @GetMapping("/posts")
+    // public List<Post> getPostList() {
+    //     return postRepository.findAll();
+    // }
+    
+    // 2-2 글 목록을 페이징하여 반환
     @GetMapping("/posts")
     public List<Post> getPostList() {
         return postRepository.findAll(
             PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
         );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
-    
+
     // 3. 글 번호로 조회
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable("id") Long id) {
+        return postRepository.findById(id).get();
+    }
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
